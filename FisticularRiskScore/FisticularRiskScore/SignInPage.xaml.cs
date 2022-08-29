50ed37518772751f376ff49483808b379664d894
Tsurcan Alexandr <Brain.Cawa@gmail.com>
2022-08-29 17:33:12 +0300

    Email validation

--- FisticularRiskScore/FisticularRiskScore/SignInPage.xaml.cs
+++ FisticularRiskScore/FisticularRiskScore/SignInPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
@@ -12,6 +13,8 @@ namespace FisticularRiskScore
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SignInPage : ContentPage
     {
+        private Regex EmailRegex = new Regex(@"^[a-z,A-Z]{1,10}((-|.)\w+)*@\w+.\w[a-z,A-Z]{1,3}$");
+
         public SignInPage()
         {
             InitializeComponent();
@@ -20,6 +23,20 @@ namespace FisticularRiskScore
         private async void OnSignUp(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new SignUpPage());
+        }
+
+        private void OnEntriesValidation(object sender, TextChangedEventArgs e)
+        {
+            if (EmailRegex.IsMatch(mailEntry.Text))
+            {
+                mailEntry.TextColor = Color.Default;
+                labelMailError.Text = "";
+            }
+            else
+            {
+                mailEntry.TextColor = Color.Red;
+                labelMailError.Text = "Invalid email adress";
+            }
         }
     }
 }
