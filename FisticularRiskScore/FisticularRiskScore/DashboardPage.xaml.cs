cd4f15a917b0e1d0c644caea41790557af0c7731
Tsurcan Alexandr <Brain.Cawa@gmail.com>
2022-09-08 15:47:48 +0300

    Added picker for language selection

--- FisticularRiskScore/FisticularRiskScore/DashboardPage.xaml.cs
+++ FisticularRiskScore/FisticularRiskScore/DashboardPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using static FisticularRiskScore.LanguagePage;
 
 namespace FisticularRiskScore
 {
@@ -17,6 +19,21 @@ namespace FisticularRiskScore
             var response = await DisplayAlert("", "Are you sure to exit", "Yes", "No");
             if (response)
                 await Navigation.PopAsync();
+        }
+
+        private void OnSelectingLanguage(object sender, EventArgs e)
+        {
+            var page = new LanguagePage();
+            page.LanguageSelection.ItemSelected += (source, args) =>
+            {
+                var selectedItem = args.SelectedItem as Language;
+                languageSelected.Text = selectedItem.Name;
+                //return new System.Globalization.CultureInfo(selectedItem.Sign);
+
+                Navigation.PopAsync();
+                //Debug.WriteLine(selectedItem.Sign);
+            };
+            Navigation.PushAsync(page);
         }
     }
 }
---
+++ FisticularRiskScore/FisticularRiskScore/LanguagePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FisticularRiskScore
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LanguagePage : ContentPage
+    {
+        public List<Language> Languages { get; set; }
+
+        public class Language
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Sign { get; set; }
+        }
+
+        public LanguagePage()
+        {
+            InitializeComponent();
+
+            languageList.ItemsSource = new List<Language>
+            {
+                new Language {Id = 1, Name = "English", Sign="en-US" },
+                new Language {Id = 2, Name = "Russian", Sign="ru-RU" }
+            };
+        }
+
+        public ListView LanguageSelection
+        { get { return languageList; } }
+    }
+}
